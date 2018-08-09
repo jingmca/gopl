8c3ca47b53120aaf22a4198b7444901bb900f828
🐶 <jm@deMacBook-Pro.local>
2018-08-09 17:07:17 +0800

    echo duplicated line and its line num from each input file

--- gopl/dup.go
+++ gopl/dup.go
@@ -3,19 +3,68 @@ package gopl
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
+
+type record struct {
+	filename string
+	linenu   []int
+}
+
+type recordList []record
 
 //Dup func() will lookup duplicate lines from input
 func Dup() {
-	for _, files := range os.Args[1:] {
-		if len(files) == 0 {
+
+	counts := make(map[string]int)
+	records := make(map[string]recordList)
+
+	for _, file := range os.Args[1:] {
+		if len(file) == 0 {
 			fmt.Println("dup: no files")
-			return
+			continue
 		}
-		fmt.Printf("%T, %s", files, files)
-		for _, file := range files {
-			fmt.Println(file)
+		f, err := ioutil.ReadFile(file)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup: %s\n", err)
+			continue
+		}
+		for nu, line := range strings.Split(string(f), "\n") {
+			counts[line]++
+			if len(records[line]) == 0 {
+				records[line] = recordList{record{file, []int{nu + 1}}}
+			} else {
+				addRecord(&records, &line, &file, nu)
+			}
+
 		}
 	}
+
+	for line, n := range counts {
+		fmt.Printf("------\nNUMS[%d]\tTEXT:<%s>\n", n, line)
+		for _, list := range records[line] {
+			fmt.Printf("FILE: <%s>\tLINE: %v\n", list.filename, list.linenu)
+		}
+	}
+}
+
+func addRecord(list *map[string]recordList, line *string, filename *string, linenu int) bool {
+
+	ret := false
+	alist := (*list)[*line]
+	for i, item := range alist {
+		if item.filename == *filename {
+			(*list)[*line][i].linenu = append(item.linenu, linenu+1)
+			ret = true
+			break
+		}
+	}
+	if ret == false {
+		var tmp = record{*filename, []int{linenu + 1}}
+		(*list)[*line] = append(alist, tmp)
+	}
+
+	return ret
 }
