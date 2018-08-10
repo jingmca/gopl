1971fcffd1322378175e53cb765634ede03659a9
🐶 <jm@deMacBook-Pro.local>
2018-08-10 11:41:12 +0800

    add two prof func

--- echo.go
+++ echo.go
@@ -3,7 +3,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"runtime/pprof"
+	"time"
 
 	gopl "github.com/jingmca/gopl"
 )
@@ -16,4 +19,32 @@ func echo() {
 
 func main() {
 	gopl.Dup()
+}
+
+func cpuProfile() {
+	f, err := os.OpenFile("cpu.prof", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	log.Println("CPU Profile started")
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
+	time.Sleep(60 * time.Second)
+	fmt.Println("CPU Profile stopped")
+}
+
+func heapProfile() {
+	f, err := os.OpenFile("heap.prof", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	time.Sleep(30 * time.Second)
+
+	pprof.WriteHeapProfile(f)
+	fmt.Println("Heap Profile generated")
 }
