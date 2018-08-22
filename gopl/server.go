ce32042dd5b28926ed4eb45540cea549fc7469de
🐶 <jm@deMacBook-Pro.local>
2018-08-22 19:08:33 +0800

    httpd v2

--- gopl/server.go
+++ gopl/server.go
@@ -5,13 +5,44 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 )
+
+var mu sync.Mutex
+var count int
 
 //App will show
 func App() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(os.Stdout, "%s %s %s\n", r.RemoteAddr, r.URL.Path, "200")
 		fmt.Fprintf(w, "URL.Path = %s", r.URL.Path)
+		mu.Lock()
+		count++
+		mu.Unlock()
+
+	})
+
+	http.HandleFunc("/count", func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		defer mu.Unlock()
+		fmt.Fprintf(w, "Requests:%3d", count)
+	})
+
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "123")
+	})
+
+	http.HandleFunc("/header", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+		for k, v := range r.Header {
+			fmt.Fprintf(w, "Header [%q] = %q\n", k, v)
+		}
+		if err := r.ParseForm(); err != nil {
+			log.Print(err)
+		}
+		for k, v := range r.Form {
+			fmt.Fprintf(w, "Form [%q] = %q\n", k, v)
+		}
 	})
 
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
